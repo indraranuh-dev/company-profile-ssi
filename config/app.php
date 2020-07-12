af432c9fb6f1b6f96b9a7f1dd1026d63ee9f5525
indraranuh-dev <61109346+indraranuh-dev@users.noreply.github.com>
2020-07-12 20:13:09 +0700

    Disable package page speed (sementara)

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          */
         Spatie\Permission\PermissionServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
-        RenatoMarinho\LaravelPageSpeed\ServiceProvider::class,
+        // RenatoMarinho\LaravelPageSpeed\ServiceProvider::class,
 
         /*
          * Application Service Providers...
