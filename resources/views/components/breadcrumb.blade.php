60240e307a95a93b57a01b1675188ff0911331fe
indraranuh-dev <61109346+indraranuh-dev@users.noreply.github.com>
2020-07-12 11:39:40 +0700

    Menambahkan component breadcrumb

---
+++ resources/views/components/breadcrumb.blade.php
@@ -0,0 +1,10 @@
+<div class="page-breadcrumb">
+    <div class="row">
+        <div class="col-12">
+            <h4 class="page-title">Jenis Produk</h4>
+            <nav aria-label="breadcrumb">
+                {{$slot}}
+            </nav>
+        </div>
+    </div>
+</div>
