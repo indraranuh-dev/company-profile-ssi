ed1fccec9dc8581b774a1359138166ae77174f14
indraranuh-dev <61109346+indraranuh-dev@users.noreply.github.com>
2020-07-13 13:47:31 +0700

    Menambahkan variabel baru pada component breadbrumb

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,7 +1,7 @@
 <div class="page-breadcrumb">
     <div class="row">
         <div class="col-12">
-            <h4 class="page-title">Jenis Produk</h4>
+            <h4 class="page-title">{{$title}}</h4>
             <nav aria-label="breadcrumb">
                 {{$slot}}
             </nav>
