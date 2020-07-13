c8a96297d6974a199c1855b38cf64ba84fea9705
indraranuh-dev <61109346+indraranuh-dev@users.noreply.github.com>
2020-07-13 18:54:18 +0700

    Menghapus pakace datatables yajra

--- config/app.php
+++ config/app.php
@@ -166,7 +166,6 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
-        Yajra\DataTables\DataTablesServiceProvider::class,
         // RenatoMarinho\LaravelPageSpeed\ServiceProvider::class,
 
         /*
@@ -230,8 +229,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
-
     ],
 
 ];
